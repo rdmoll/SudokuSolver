b9593479cd7f5ef4137c591d10a95b426fc8d20d
Ryan Moll <ryan.d.moll@gmail.com>
2020-05-23 02:29:14 -0400

    More updates

--- SudokoSolver.py
+++ SudokoSolver.py
@@ -1,135 +1,51 @@
 # Sudoku Solver
 
-class cell:
+from cell import *
 
-    def __init__( self, row, col ):
-        self.row = row
-        self.col = col
-        self.FinalValue = 0
-        self.rowStart = 3*(row // 3)
-        self.rowEnd = self.rowStart + 2
-        self.colStart = 3*(col // 3)
-        self.colEnd = self.colStart + 2
-        self.impossible = []
-        self.possible = []
-
-    def determineImpossible( self, gridVals ):
-        for i in range( 0, 9 ):
-            if (i != self.col):
-                val = gridVals[self.row][i]
-                if ( (val != 0) and not(val in self.impossible) ):
-                    self.impossible.append( val )
-
-        for i in range( 0, 9 ):
-            if (i != self.row):
-                val = gridVals[i][self.col]
-                if ( (val != 0) and not(val in self.impossible) ):
-                    self.impossible.append( val )
-
-        for k in range( self.rowStart, self.rowEnd+1 ):
-            for i in range( self.colStart, self.colEnd+1 ):
-                if (i != self.col) and (k != self.row):
-                    val = gridVals[k][i]
-                    if ( (val != 0) and not(val in self.impossible) ):
-                        self.impossible.append( val )
-
-        if ( len(self.impossible) == 8 ):
-            for i in range( 1, 10 ):
-                if not(i in self.impossible):
-                    self.FinalValue = i
-
-    def determinePossible( self, gridCells ):
-        for j in range( 1, 10 ):
-            if not(j in self.impossible):
-
-                excludedFromOthers = True
-                for i in range( 0, 9 ):
-                    if (i != self.col) and (gridCells[self.row][i].FinalValue == 0):
-                        excludedFromOthers = excludedFromOthers and (j in gridCells[self.row][i].impossible)
-                if excludedFromOthers:
-                    print("test1")
-                    self.FinalValue = j
-
-                excludedFromOthers = True
-                for i in range( 0, 9 ):
-                    if (i != self.row) and (gridCells[i][self.col].FinalValue == 0):
-                        excludedFromOthers = excludedFromOthers and (j in gridCells[i][self.col].impossible)
-                if excludedFromOthers:
-                    print("test2")
-                    self.FinalValue = j
-
-                excludedFromOthers = True
-                for k in range( self.rowStart, self.rowEnd+1 ):
-                    for i in range( self.colStart, self.colEnd+1 ):
-                        if (i != self.col) and (k != self.row) and (gridCells[k][i].FinalValue == 0):
-                            excludedFromOthers = excludedFromOthers and (j in gridCells[k][i].impossible)
-                if excludedFromOthers:
-                    print("test3, ",self.row,", ",self.col)
-                    self.FinalValue = j
-
-def main():
-    gridVals  = [ [ 0 for j in range( 0, 9 ) ] for i in range( 0, 9 ) ]
-    gridCells = [ [ cell( i, j ) for j in range( 0, 9 ) ] for i in range( 0, 9 ) ]
-
-    #gridVals[0][1] = 4; gridVals[0][3] = 3; gridVals[0][4] = 5; gridVals[0][5] = 8;
-    #gridVals[1][0] = 8; gridVals[1][3] = 4; gridVals[1][4] = 7; gridVals[1][7] = 5;
-    #gridVals[2][0] = 5; gridVals[2][2] = 1; gridVals[2][4] = 6; gridVals[2][6] = 8; gridVals[2][8] = 7;
-    #gridVals[3][2] = 3; gridVals[3][3] = 6; gridVals[3][4] = 1; gridVals[3][6] = 2;
-    #gridVals[4][0] = 2; gridVals[4][8] = 6;
-    #gridVals[5][2] = 8; gridVals[5][4] = 3; gridVals[5][5] = 2; gridVals[5][6] = 5;
-    #gridVals[6][0] = 3; gridVals[6][2] = 2; gridVals[6][4] = 8; gridVals[6][6] = 7; gridVals[6][8] = 4;
-    #gridVals[7][1] = 8; gridVals[7][4] = 4; gridVals[7][5] = 7; gridVals[7][8] = 3;
-    #gridVals[8][3] = 9; gridVals[8][4] = 2; gridVals[8][5] = 3; gridVals[8][7] = 8;
-
-    gridVals[0][8] = 8;
-    gridVals[1][0] = 6; gridVals[1][3] = 9; gridVals[1][5] = 3; gridVals[1][6] = 4;
-    gridVals[2][5] = 7; gridVals[2][7] = 9; gridVals[2][8] = 1;
-    gridVals[3][1] = 4; gridVals[3][7] = 2; gridVals[3][8] = 5;
-    gridVals[4][1] = 5; gridVals[4][3] = 8; gridVals[4][4] = 4; gridVals[4][5] = 6; gridVals[4][7] = 3;
-    gridVals[5][0] = 9; gridVals[5][1] = 6; gridVals[5][7] = 1;
-    gridVals[6][0] = 2; gridVals[6][1] = 8; gridVals[6][3] = 7;
-    gridVals[7][2] = 6; gridVals[7][3] = 2; gridVals[7][5] = 4; gridVals[7][8] = 9;
-    gridVals[8][0] = 7;
-
+def print_grid(val_grid):
     for i in range( 0, 9 ):
         for j in range( 0, 9 ):
-            print(gridVals[i][j], end = '  ')
+            print(val_grid[i][j], end = '  ')
         print()
-
     print()
 
-    changes = True
-    for k in range( 0, 100 ):
-        numZeros = 0
-        for i in range( 0, 9 ):
-            for j in range( 0, 9 ):
-                if gridVals[i][j] == 0:
-                    numZeros = numZeros + 1
-                    gridCells[i][j].determineImpossible( gridVals )
-                    gridCells[i][j].determinePossible( gridCells )
-                    gridVals[i][j] = gridCells[i][j].FinalValue
+def main():
+    val_grid = [ [ 0 for j in range( 0, 9 ) ] for i in range( 0, 9 ) ]
 
-        for i in range( 0, 9 ):
-            for j in range( 0, 9 ):
-                print(gridVals[i][j], end = '  ')
-            print()
+    #val_grid[0][1] = 4; val_grid[0][3] = 3; val_grid[0][4] = 5; val_grid[0][5] = 8;
+    #val_grid[1][0] = 8; val_grid[1][3] = 4; val_grid[1][4] = 7; val_grid[1][7] = 5;
+    #val_grid[2][0] = 5; val_grid[2][2] = 1; val_grid[2][4] = 6; val_grid[2][6] = 8; val_grid[2][8] = 7;
+    #val_grid[3][2] = 3; val_grid[3][3] = 6; val_grid[3][4] = 1; val_grid[3][6] = 2;
+    #val_grid[4][0] = 2; val_grid[4][8] = 6;
+    #val_grid[5][2] = 8; val_grid[5][4] = 3; val_grid[5][5] = 2; val_grid[5][6] = 5;
+    #val_grid[6][0] = 3; val_grid[6][2] = 2; val_grid[6][4] = 8; val_grid[6][6] = 7; val_grid[6][8] = 4;
+    #val_grid[7][1] = 8; val_grid[7][4] = 4; val_grid[7][5] = 7; val_grid[7][8] = 3;
+    #val_grid[8][3] = 9; val_grid[8][4] = 2; val_grid[8][5] = 3; val_grid[8][7] = 8;
 
-        input()
+    val_grid[0][8] = 8;
+    val_grid[1][0] = 6; val_grid[1][3] = 9; val_grid[1][5] = 3; val_grid[1][6] = 4;
+    val_grid[2][5] = 7; val_grid[2][7] = 9; val_grid[2][8] = 1;
+    val_grid[3][1] = 4; val_grid[3][7] = 2; val_grid[3][8] = 5;
+    val_grid[4][1] = 5; val_grid[4][3] = 8; val_grid[4][4] = 4; val_grid[4][5] = 6; val_grid[4][7] = 3;
+    val_grid[5][0] = 9; val_grid[5][1] = 6; val_grid[5][7] = 1;
+    val_grid[6][0] = 2; val_grid[6][1] = 8; val_grid[6][3] = 7;
+    val_grid[7][2] = 6; val_grid[7][3] = 2; val_grid[7][5] = 4; val_grid[7][8] = 9;
+    val_grid[8][0] = 7;
 
-        if (numZeros == 0):
-            print("Completed in: ",k," iterations")
-            print()
-            break
+    print_grid(val_grid)
 
-        if (k == 24) and (numZeros != 0):
-            print()
-            print("Puzzle not solved.")
-            print()
+    cell_grid = [ [ Cell(i, j, val_grid[i][j]) for j in range(0, 9) ] for i in range(0, 9) ]
 
-    for i in range( 0, 9 ):
-        for j in range( 0, 9 ):
-            print(gridVals[i][j], end = '  ')
-        print()
+    for k in range(0, 5):
+        for i in range(0, 9):
+            for j in range(0, 9):
+                if (cell_grid[i][j] == 0):
+                    cell_grid[i][j].update_possible(val_grid)
+                    cell_grid[i][j].update_unique(cell_grid)
+                    if (cell_grid[i][j] != 0):
+                        val_grid[i][j] = cell_grid[i][j].value
+
+        #print_grid(val_grid)
 
 if __name__ == "__main__":
     main()
--- cell.py
+++ cell.py
@@ -2,11 +2,58 @@ import numpy as np
 
 class Cell:
 
-    def __init __(self, row, col, value):
+    def __init__(self, row, col, value):
         self.row = row
         self.col = col
         self.value = value
-        self.possible = np.arange(1,10)
+        self.possible_values = np.arange(1,10)
 
-    def update(self, grid):
-        
+    def update_possible(self, val_grid):
+        for i in range(0, 9):
+            if (i != self.col):
+                self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[self.row][i]))
+
+        for i in range(0, 9):
+            if (i != self.row):
+                self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[i][self.col]))
+
+        self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[self.row + 1][self.col + 1]))
+        self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[self.row + 1][self.col - 1]))
+        self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[self.row - 1][self.col + 1]))
+        self.possible_values = np.delete(self.possible_values, np.argwhere(self.possible_values == val_grid[self.row - 1][self.col - 1]))
+
+        if (self.possible_values == 1):
+            self.value = self.possible_values[0]
+
+    def update_unique(self, cell_grid):
+        for pv in self.possible_values:
+
+            unique = True
+            for i in range(0, 9):
+                if (i != self.col and cell_grid[self.row][i].value == 0):
+                    if pv in cell_grid[self.row][i].possible_values:
+                        unique = False
+
+            for i in range(0, 9):
+                if (i != self.row and cell_grid[i][self.col].value == 0):
+                    if pv in cell_grid[i][self.col].possible_values:
+                        unique = False
+
+            if (cell_grid[self.row + 1][self.col + 1].value == 0):
+                if pv in cell_grid[self.row + 1][self.col + 1].possible_values:
+                    unique = False
+
+            if (cell_grid[self.row + 1][self.col - 1].value == 0):
+                if pv in cell_grid[self.row + 1][self.col - 1].possible_values:
+                    unique = False
+
+            if (cell_grid[self.row - 1][self.col + 1].value == 0):
+                if pv in cell_grid[self.row - 1][self.col + 1].possible_values:
+                    unique = False
+
+            if (cell_grid[self.row - 1][self.col - 1].value == 0):
+                if pv in cell_grid[self.row - 1][self.col - 1].possible_values:
+                    unique = False
+
+            if (unique):
+                self.value = pv
