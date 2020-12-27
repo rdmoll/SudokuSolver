3cfaddce5131363f62f3330831b2fd1368c34a6f
rdmoll <ryan.d.moll@gmail.com>
2020-12-27 00:30:15 -0500

    Updating with csv reader

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,8 +1,10 @@
 # SudokuSolver
-
+import numpy as np
+import pandas as pd
 from cell import *
 from divData import *
 
+# Function for printing grid
 def print_grid(val_grid):
     for i in range( 0, 9 ):
         for j in range( 0, 9 ):
@@ -11,42 +13,32 @@ def print_grid(val_grid):
     print()
 
 def main():
-    val_grid = [ [ 0 for j in range( 0, 9 ) ] for i in range( 0, 9 ) ]
 
-    val_grid[0][1] = 4; val_grid[0][3] = 3; val_grid[0][4] = 5; val_grid[0][5] = 8;
-    val_grid[1][0] = 8; val_grid[1][3] = 4; val_grid[1][4] = 7; val_grid[1][7] = 5;
-    val_grid[2][0] = 5; val_grid[2][2] = 1; val_grid[2][4] = 6; val_grid[2][6] = 8; val_grid[2][8] = 7;
-    val_grid[3][2] = 3; val_grid[3][3] = 6; val_grid[3][4] = 1; val_grid[3][6] = 2;
-    val_grid[4][0] = 2; val_grid[4][8] = 6;
-    val_grid[5][2] = 8; val_grid[5][4] = 3; val_grid[5][5] = 2; val_grid[5][6] = 5;
-    val_grid[6][0] = 3; val_grid[6][2] = 2; val_grid[6][4] = 8; val_grid[6][6] = 7; val_grid[6][8] = 4;
-    val_grid[7][1] = 8; val_grid[7][4] = 4; val_grid[7][5] = 7; val_grid[7][8] = 3;
-    val_grid[8][3] = 9; val_grid[8][4] = 2; val_grid[8][5] = 3; val_grid[8][7] = 8;
+    # Read initial grid from csv file
+    val_grid_trans = pd.read_csv("inputGrid.csv",header=None)
+    val_grid = np.transpose(val_grid_trans)
 
-    #val_grid[0][8] = 8;
-    #val_grid[1][0] = 6; val_grid[1][3] = 9; val_grid[1][5] = 3; val_grid[1][6] = 4;
-    #val_grid[2][5] = 7; val_grid[2][7] = 9; val_grid[2][8] = 1;
-    #val_grid[3][1] = 4; val_grid[3][7] = 2; val_grid[3][8] = 5;
-    #val_grid[4][1] = 5; val_grid[4][3] = 8; val_grid[4][4] = 4; val_grid[4][5] = 6; val_grid[4][7] = 3;
-    #val_grid[5][0] = 9; val_grid[5][1] = 6; val_grid[5][7] = 1;
-    #val_grid[6][0] = 2; val_grid[6][1] = 8; val_grid[6][3] = 7;
-    #val_grid[7][2] = 6; val_grid[7][3] = 2; val_grid[7][5] = 4; val_grid[7][8] = 9;
-    #val_grid[8][0] = 7;
-
-    print_grid(val_grid)
-
+    # Initialize grid cells, rows, cols, and tiles
     cell_grid = [ [ Cell(i, j, val_grid[i][j]) for j in range(0, 9) ] for i in range(0, 9) ]
     rows = [ RowData(i) for i in range(0, 9) ]
     cols = [ ColData(i) for i in range(0, 9) ]
     tiles = [ [ TileData(i, j) for j in range(0, 3) ] for i in range(0, 3) ]
 
+    # Print initial grid
+    print_grid(val_grid)
+    print()
+
+    # Loop through checks
     for t in range(0,10):
 
+        # Find grid cells with only 1 possible values
         for i in range(0, 9):
             for j in range(0, 9):
                 if (cell_grid[i][j].value == 0):
                     cell_grid[i][j].update_possible(val_grid)
 
+        # Find missing values in rows, cols, and tiles which are only
+        # possible for one cell
         for i in range(0, 9):
             rows[i].update_missing(cell_grid)
             cols[i].update_missing(cell_grid)
@@ -54,11 +46,12 @@ def main():
             for j in range(0, 3):
                 tiles[i][j].update_missing(cell_grid)
 
+        # Update val_grid
         for i in range(0, 9):
             for j in range(0, 9):
                 val_grid[i][j] = cell_grid[i][j].value
 
-    print()
+    # Print result grid, run accuracy checks
     print_grid(val_grid)
     print()
     for i in range(0, 9):
